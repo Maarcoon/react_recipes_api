3e34a1f7ef8b35751751e20d59aaa1c8726c55b6
Felipe Marcon <felipe@growthhackers.com>
2020-03-09 22:39:52 -0300

    Add ingredients and some style to our recipes

--- src/App.js
+++ src/App.js
@@ -45,13 +45,16 @@ function App() {
         </button>
       </form>
 
-      {recipes.map(recipe =>(
-        <Recipe
-          key={recipe.recipe.label}
-          title={recipe.recipe.label}
-          calories={recipe.recipe.calories}
-          image={recipe.recipe.image} />
-      ))}
+      <div className='recipes'>
+        {recipes.map(recipe =>(
+          <Recipe
+            key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients} />
+        ))}
+      </div>
     </div>
   );
 }
